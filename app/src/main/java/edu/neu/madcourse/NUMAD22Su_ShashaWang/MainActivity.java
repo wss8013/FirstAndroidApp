8b926b0eeab80085592eac13558a9ec025c666f0
wss8013 <wss8013@gmail.com>
2022-05-14 22:52:09 -0700

    add edit text

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.about_me);
+        EditText edit = (EditText)findViewById(R.id.my_edit);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Context context = getApplicationContext();
-                CharSequence text = "Name: Shasha Wang \n Email: wang.shas@northeastern.edu";
+                CharSequence text = "Name: Shasha Wang \n Email: wang.shas@northeastern.edu \n";
                 int duration = Toast.LENGTH_SHORT;
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
