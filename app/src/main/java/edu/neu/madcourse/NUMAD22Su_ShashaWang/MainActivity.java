41de0c4acd9956b6de437654edbdb8f57771f3fa
wss8013 <wss8013@gmail.com>
2022-05-14 22:36:16 -0700

    Add toast to button click listener.  Also change button text to about me

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/MainActivity.java
@@ -2,7 +2,11 @@ package edu.neu.madcourse.NUMAD22Su_ShashaWang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.about_me);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Name: Shasha Wang \n Email: wang.shas@northeastern.edu";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 }
