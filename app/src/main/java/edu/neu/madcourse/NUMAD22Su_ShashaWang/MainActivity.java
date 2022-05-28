e78f8602da0cd63cdeec5277e1886e17cc57e98b
wss8013 <wss8013@gmail.com>
2022-05-27 23:17:00 -0700

    add activity and constraint layout

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/MainActivity.java
@@ -3,6 +3,7 @@ package edu.neu.madcourse.NUMAD22Su_ShashaWang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,17 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.about_me);
+        Button aboutMe = (Button) findViewById(R.id.about_me);
+        Button clicky = (Button) findViewById(R.id.clicky);
+
         EditText edit = (EditText)findViewById(R.id.my_edit);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = "Name: Shasha Wang \n Email: wang.shas@northeastern.edu \n";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
+        aboutMe.setOnClickListener(v -> {
+            Context context = getApplicationContext();
+            CharSequence text = "Name: Shasha Wang \n Email: wang.shas@northeastern.edu \n";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         });
+        clicky.setOnClickListener(v->{
+            Intent intent = new Intent(this, ClickyActivity.class);
+            startActivity(intent);
+        });
+
 
     }
 }
