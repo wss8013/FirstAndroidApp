341f65c8ebe0b1bcccf427f9fb1453616e0ecdba
wss8013 <wss8013@gmail.com>
2022-05-27 23:42:04 -0700

    add text view for displaying button text

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/ClickyActivity.java
@@ -1,6 +1,7 @@
 package edu.neu.madcourse.NUMAD22Su_ShashaWang;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,11 @@ public class ClickyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+        Button buttonA = findViewById(R.id.a);
+        Button buttonB = findViewById(R.id.b);
+        Button buttonC = findViewById(R.id.c);
+        Button buttonD = findViewById(R.id.d);
+        Button buttonE = findViewById(R.id.e);
+        Button buttonF = findViewById(R.id.f);
     }
 }
