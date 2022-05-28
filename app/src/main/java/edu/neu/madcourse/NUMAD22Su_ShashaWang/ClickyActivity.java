654c7cd1f9a55f1973f199ec34a089d8ab5d51fb
wss8013 <wss8013@gmail.com>
2022-05-28 00:01:04 -0700

    adjust text view to center of the screen

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/ClickyActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/ClickyActivity.java
@@ -2,6 +2,7 @@ package edu.neu.madcourse.NUMAD22Su_ShashaWang;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,5 +18,17 @@ public class ClickyActivity extends AppCompatActivity {
         Button buttonD = findViewById(R.id.d);
         Button buttonE = findViewById(R.id.e);
         Button buttonF = findViewById(R.id.f);
+        TextView buttonText = findViewById(R.id.pressed);
+        buttonA.setOnClickListener(v->setButtonText((Button)v,buttonText ));
+        buttonB.setOnClickListener(v->setButtonText((Button)v,buttonText ));
+        buttonC.setOnClickListener(v->setButtonText((Button)v,buttonText ));
+        buttonD.setOnClickListener(v->setButtonText((Button)v,buttonText ));
+        buttonE.setOnClickListener(v->setButtonText((Button)v,buttonText ));
+        buttonF.setOnClickListener(v->setButtonText((Button)v,buttonText ));
+
+    }
+
+    private void setButtonText(Button button,TextView textView ) {
+        textView.setText("Pressed: " + button.getText());
     }
 }
