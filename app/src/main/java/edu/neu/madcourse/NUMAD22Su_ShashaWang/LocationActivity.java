213cea8251d0987e65b3c5da1d8900b856707a71
wss8013 <wss8013@gmail.com>
2022-06-18 11:58:02 -0700

    add location distance calculation

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/LocationActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/LocationActivity.java
@@ -3,21 +3,23 @@ package edu.neu.madcourse.NUMAD22Su_ShashaWang;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 
 
-public class LocationActivity extends AppCompatActivity implements
-        LocationListener {
+public class LocationActivity extends AppCompatActivity {
     private TextView currentLatitude;
     private TextView currentLongitude;
     private TextView travelDistance;
@@ -26,6 +28,9 @@ public class LocationActivity extends AppCompatActivity implements
     private double latitudeValue;
     private double longitudeValue;
     private double distanceValue;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,32 +41,51 @@ public class LocationActivity extends AppCompatActivity implements
         travelDistance = findViewById(R.id.total_distance);
         latitudeValue = 0.0f;
         longitudeValue = 0.0f;
-        if(savedInstanceState != null &&savedInstanceState.containsKey("distance")) {
+        if (savedInstanceState != null && savedInstanceState.containsKey("distance")) {
             System.out.println(" shasha saving instance value ");
             distanceValue = savedInstanceState.getDouble("distance");
-        }else {
+        } else {
             distanceValue = 0.0f;
         }
         currentLatitude.setText("Current latitude: " + latitudeValue);
         currentLongitude.setText("current longitude: " + longitudeValue);
-        travelDistance.setText("total_distance: " + distanceValue);
+        travelDistance.setText("total_distance: " + String.format("%.2f", distanceValue) + "M");
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                && ActivityCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
             requestPermissions(
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION},
                     /*requestCode=*/1);
             return;
         }
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         fusedLocationClient.getLastLocation().addOnSuccessListener(this, this::setLocation);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                System.out.println("jerry here get location update "
+                        + locationResult.getLastLocation().toString());
+                onLocationChanged(locationResult.getLastLocation());
+            }
+        };
+        locationRequest = locationRequest.create();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(50);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
+
     }
 
     private void setLocation(Location location) {
-
         // Got last known location. In some rare situations this can be null.
         if (location == null) {
             currentLatitude.setText("Error getting location");
@@ -74,7 +98,7 @@ public class LocationActivity extends AppCompatActivity implements
         System.out.println("set location here here here ");
         currentLatitude.setText("Current latitude:" + latitudeValue);
         currentLongitude.setText("current longitude: " + longitudeValue);
-        travelDistance.setText("total_distance:" + distanceValue);
+        travelDistance.setText("total_distance:" + String.format("%.2f", distanceValue) + "M");
     }
 
     @Override
@@ -96,9 +120,29 @@ public class LocationActivity extends AppCompatActivity implements
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+
+    }
+
+    private void startLocationUpdates() {
+
+    }
 
     @Override
-    public void onLocationChanged(@NonNull Location location) {
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
+
+    public void onLocationChanged(Location location) {
         if (location == null) {
             currentLatitude.setText("Error getting location");
             currentLongitude.setText("Error getting location");
@@ -107,13 +151,15 @@ public class LocationActivity extends AppCompatActivity implements
         }
         double newLongitude = location.getLongitude();
         double newLatitude = location.getLatitude();
-        distanceValue += Math.sqrt(Math.pow(latitudeValue - newLatitude, 2)
-                + Math.pow(longitudeValue - newLongitude, 2));
+        float[] res = new float[1];
+        Location.distanceBetween(latitudeValue, longitudeValue, newLatitude, newLongitude, res);
+        distanceValue += res[0];
+
         latitudeValue = newLatitude;
         longitudeValue = newLongitude;
         currentLatitude.setText("Current latitude:" + latitudeValue);
         currentLongitude.setText("current longitude: " + longitudeValue);
-        travelDistance.setText("total_distance:" + distanceValue);
+        travelDistance.setText("total_distance:" + String.format("%.2f", distanceValue) + "M");
     }
 
     @Override
