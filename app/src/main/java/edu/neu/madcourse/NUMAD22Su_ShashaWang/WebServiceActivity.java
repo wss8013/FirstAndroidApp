13526b23ce56fdb9ea9e03f4b9d08070a20ddf8c
wss8013 <wss8013@gmail.com>
2022-06-26 12:23:14 -0700

    convert async Task to native java thread

--- app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/WebServiceActivity.java
+++ app/src/main/java/edu/neu/madcourse/NUMAD22Su_ShashaWang/WebServiceActivity.java
@@ -3,8 +3,8 @@ package edu.neu.madcourse.NUMAD22Su_ShashaWang;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -31,6 +31,8 @@ public class WebServiceActivity extends AppCompatActivity {
     ImageView dogImage;
     Spinner breedSpinner;
     ProgressBar progressBar;
+    private Handler imageHandler = new Handler();
+    private Handler breedListHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,133 +41,102 @@ public class WebServiceActivity extends AppCompatActivity {
         dogImage = findViewById(R.id.dog);
         breedSpinner = findViewById(R.id.breed_spinner);
         progressBar = findViewById(R.id.loading_bar);
-//        progressBar.setVisibility(View.VISIBLE);
-//        dogImage.setVisibility(View.GONE);
-        new DownloadBreedTask(breedSpinner, this.getApplicationContext())
-                .execute("https://dog.ceo/api/breeds/list/all");
-
+        new GetBreedListThread(this.getApplicationContext()).start();
     }
 
-    public void randomGetDogOnClick(View view) {
-        GetDogTask webServiceTask = new GetDogTask();
-        progressBar.setVisibility(View.VISIBLE);
-        dogImage.setVisibility(View.GONE);
-        webServiceTask.execute("https://dog.ceo/api/breeds/image/random");
-    }
+    //  Threads that executes web API call to get dog image
+    private class GetDogThread extends Thread {
+        private String dogApiUrl;
 
-    private class GetDogTask extends AsyncTask<String, Integer, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            URL url;
-            String dogUrl = "";
-            try {
-                url = new URL(params[0]);
-                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                InputStream is = conn.getInputStream();
-                final String res = convertStreamToString(is);
-                JSONObject jsonObject = new JSONObject(res);
-                dogUrl = jsonObject.getString("message");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return dogUrl;
+        public GetDogThread(String dogApiUrl) {
+            this.dogApiUrl = dogApiUrl;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            new DownloadImageTask(dogImage).execute(s);
+        public void run() {
+            String dogUrl = getDogImageUrl(dogApiUrl);
+            Bitmap image = downloadDogImage(dogUrl);
+            // set image using handler
+            imageHandler.post(() -> {
+                progressBar.setVisibility(View.GONE);
+                dogImage.setImageBitmap(image);
+                dogImage.setVisibility(View.VISIBLE);
+            });
         }
     }
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
+    protected String getDogImageUrl(String dogApiUrl) {
+        URL url;
+        String dogUrl = "";
+        try {
+            url = new URL(dogApiUrl);
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            InputStream is = conn.getInputStream();
+            final String res = convertStreamToString(is);
+            JSONObject jsonObject = new JSONObject(res);
+            dogUrl = jsonObject.getString("message");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        @Override
-        protected Bitmap doInBackground(String... urls) {
-            String urlDisplay = urls[0];
-            Bitmap myBitmap = null;
-            try {
-                URL urlConnection = new URL(urlDisplay);
-                HttpsURLConnection connection = (HttpsURLConnection) urlConnection
-                        .openConnection();
-                connection.setDoInput(true);
-                connection.connect();
-                InputStream input = connection.getInputStream();
-                myBitmap = BitmapFactory.decodeStream(input);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return myBitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap result) {
-            super.onPostExecute(result);
-            progressBar.setVisibility(View.GONE);
-            bmImage.setVisibility(View.VISIBLE);
-            bmImage.setImageBitmap(result);
-
-        }
+        return dogUrl;
     }
 
-    private class DownloadBreedTask extends AsyncTask<String, Void, String[]> {
-        Spinner breedSpinner;
-        Context context;
+    protected Bitmap downloadDogImage(String dogImageUrl) {
+        Bitmap myBitmap = null;
+        try {
+            URL urlConnection = new URL(dogImageUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) urlConnection
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            myBitmap = BitmapFactory.decodeStream(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return myBitmap;
+    }
 
-        public DownloadBreedTask(Spinner spinner, Context context) {
-            this.breedSpinner = spinner;
+    // Thread downloads all breeds from the web API
+    private class GetBreedListThread extends Thread {
+        private Context context;
+
+        public GetBreedListThread(Context context) {
             this.context = context;
         }
 
         @Override
-        protected String[] doInBackground(String... urls) {
-            System.out.println("start downloading bread list " + urls[0]);
-            String resStrArray[];
-            String urlDisplay = urls[0];
+        public void run() {
+            List<String> resList = new ArrayList<>();
             try {
-                URL urlConnection = new URL(urlDisplay);
+                URL urlConnection = new URL("https://dog.ceo/api/breeds/list/all");
                 HttpsURLConnection connection = (HttpsURLConnection) urlConnection
                         .openConnection();
                 connection.setDoInput(true);
                 connection.connect();
                 InputStream input = connection.getInputStream();
                 String resStr = convertStreamToString(input);
-                List<String> resList = new ArrayList<>();
                 JSONObject object = new JSONObject(resStr);
                 JSONObject message = object.getJSONObject("message");
                 Iterator<String> keys = message.keys();
                 while (keys.hasNext()) {
                     resList.add(keys.next());
                 }
-                resStrArray = new String[resList.size()];
-                for (int i = 0; i < resStrArray.length; i++) {
-                    resStrArray[i] = resList.get(i);
-                }
             } catch (Exception e) {
-                resStrArray = new String[0];
                 e.printStackTrace();
             }
-            return resStrArray;
-        }
-
-        @Override
-        protected void onPostExecute(String[] result) {
-            super.onPostExecute(result);
-            ArrayAdapter<String> adapter
-                    = new ArrayAdapter<>
-                    (context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, result);
-            breedSpinner.setAdapter(adapter);
+            breedListHandler.post(() -> {
+                ArrayAdapter<String> adapter
+                        = new ArrayAdapter<>
+                        (context, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, resList);
+                breedSpinner.setAdapter(adapter);
+            });
         }
     }
 
@@ -174,10 +145,18 @@ public class WebServiceActivity extends AppCompatActivity {
         return s.hasNext() ? s.next() : "";
     }
 
+    // Button onclick listener for getting dog image by breed
     public void breedButtonOnClick(View view) {
         String breed = breedSpinner.getSelectedItem().toString();
         progressBar.setVisibility(View.VISIBLE);
         dogImage.setVisibility(View.GONE);
-        new GetDogTask().execute("https://dog.ceo/api/breed/" + breed + "/images/random");
+        new GetDogThread("https://dog.ceo/api/breed/" + breed + "/images/random").start();
+    }
+
+    // Button onclick listener for randomly getting all dog image
+    public void randomGetDogOnClick(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+        dogImage.setVisibility(View.GONE);
+        new GetDogThread("https://dog.ceo/api/breeds/image/random").start();
     }
 }
